09bf647d0c67e2e4a02be1327422a1c7ec739e4e
arslanahmad05 <arslanahmad0075@gmail.com>
2020-01-27 01:00:37 +0500

    Createing Friendship System in this App

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcomes#index'
 end
